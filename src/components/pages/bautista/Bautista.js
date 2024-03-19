cf322162f0cd22ab4e8ed8c8f931429a5708ceba
rae bautista <cailynrae317@gmail.com>
2024-03-19 10:47:45 +0800

    Add Bautista page

---
+++ src/components/pages/bautista/Bautista.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import "./bautistastyles.css"
+
+function Bautista() {
+
+  return (
+    <div className="content">
+      <h2>Cailyn Rae Bautista SN: 2021103905</h2>
+    </div>
+  )
+}
+
+export default Bautista
